9ecbad1f210761254e34e971175117e50e40e643
ADITYA MISHRA <75035219+DEVLOPRR@users.noreply.github.com>
2022-03-14 13:39:40 +0530

    Added `--version` Flag

--- src/main.go
+++ src/main.go
@@ -1,20 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"bread/src/commands"
-
 	"github.com/alecthomas/kong"
 )
+
+type VersionFlag bool
 
 var cli struct {
 	Install  commands.InstallCmd  `cmd:"" help:"Install an application."`
 	List     commands.ListCmd     `cmd:"" help:"List installed applications."`
 	Remove   commands.RemoveCmd   `cmd:"" help:"Remove an application."`
 	Update   commands.UpdateCmd   `cmd:"" help:"Update an application."`
+	Version  VersionFlag          `name:"version" help:"Print version information and quit"`
+}
+
+func (v VersionFlag) BeforeApply(app *kong.Kong, vars kong.Vars) error {
+	fmt.Println(vars["version"])
+	app.Exit(0)
+	return nil
 }
 
 func main() {
-	ctx := kong.Parse(&cli)
+	ctx := kong.Parse(
+		&cli,
+		kong.Name("bread"),
+		kong.Description("Install, update and remove AppImage from GitHub using your CLI."),
+		kong.UsageOnError(),
+		kong.ConfigureHelp(kong.HelpOptions{
+			Compact: true,
+		}),
+		kong.Vars{
+			"version": "Bread v0.2.2",
+		})
 	// Call the Run() method of the selected parsed command.
 	err := ctx.Run()
 	ctx.FatalIfErrorf(err)
