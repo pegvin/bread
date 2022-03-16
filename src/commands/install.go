e99104ede751cf53b96855e28e6ad02c4c257490
ADITYA MISHRA <75035219+DEVLOPRR@users.noreply.github.com>
2022-03-16 10:30:08 +0530

    Added Comment, Removed Unused Code

--- src/commands/install.go
+++ src/commands/install.go
@@ -17,43 +17,48 @@ type InstallCmd struct {
 
 // Function Which Will Be Called When `install` is the Command.
 func (cmd *InstallCmd) Run() (err error) {
-	if _, err := os.Stat(cmd.Target); err == nil {
-		cmd.createDesktopIntegration(cmd.Target)
-		return nil
-	}
-
+	// Parse The user input
 	repo, err := repos.ParseTarget(cmd.Target)
 	if err != nil {
 		return err
 	}
 
+	// Get The Latest Release
 	release, err := repo.GetLatestRelease()
 	if err != nil {
 		return err
 	}
 
+	// Show A Prompt To Select A AppImage File.
 	selectedBinary, err := utils.PromptBinarySelection(release.Files)
 	if err != nil {
 		return err
 	}
 
+	// Make A FilePath Out Of The AppImage Name
 	targetFilePath, err := utils.MakeTargetFilePath(selectedBinary)
 	if err != nil {
 		return err
 	}
 
+	// Check if the FilePath Exist, Show error
 	if _, err = os.Stat(targetFilePath); err == nil {
 		return ApplicationInstalled
 	}
 
+	// Download The AppImage
 	err = repo.Download(selectedBinary, targetFilePath)
 	if err != nil {
 		return err
 	}
 
+	// Add The Current Application To The Registry `.registry.json`
 	cmd.addToRegistry(targetFilePath, repo)
+
+	// Integrated The AppImage To Desktop
 	cmd.createDesktopIntegration(targetFilePath)
 
+	// Print Signature Info If Exist.
 	signingEntity, _ := utils.VerifySignature(targetFilePath)
 	if signingEntity != nil {
 		fmt.Println("AppImage signed by:")
@@ -66,13 +71,14 @@ func (cmd *InstallCmd) Run() (err error) {
 }
 
 // Function To Add Installed Program To Registry (Installed App information is stored in here).
-func (cmd *InstallCmd) addToRegistry(targetFilePath string, repo repos.Repo) {
-	sha1, _ := utils.GetFileSHA1(targetFilePath)
-	updateInfo, _ := utils.ReadUpdateInfo(targetFilePath)
+func (cmd *InstallCmd) addToRegistry(targetFilePath string, repo repos.Application) {
+	sha1, _ := utils.GetFileSHA1(targetFilePath) // Get The Sha1 Hash
+	updateInfo, _ := utils.ReadUpdateInfo(targetFilePath) // Get The UpdateInfo
 	if updateInfo == "" {
 		updateInfo = repo.FallBackUpdateInfo()
 	}
 
+	// Make a new entry struct
 	entry := utils.RegistryEntry{
 		FilePath:   targetFilePath,
 		Repo:       repo.Id(),
@@ -80,22 +86,22 @@ func (cmd *InstallCmd) addToRegistry(targetFilePath string, repo repos.Repo) {
 		UpdateInfo: updateInfo,
 	}
 
-	registry, _ := utils.OpenRegistry()
+	registry, _ := utils.OpenRegistry() // Open The Registry
 	if registry != nil {
-		_ = registry.Add(entry)
-		_ = registry.Close()
+		_ = registry.Add(entry) // Add the entry to registry `.registry.json`
+		_ = registry.Close() // Close the registry
 	}
 }
 
 // Function To Integrate The AppImage To Desktop. (Can Only Be Called From InstallCmd Struct)
 func (cmd *InstallCmd) createDesktopIntegration(targetFilePath string) {
-	libAppImage, err := utils.NewLibAppImageBindings()
+	libAppImage, err := utils.NewLibAppImageBindings() // Load the `libappimage` Library For Integration
 	if err != nil {
 		fmt.Println("Integration failed:", err.Error())
 		return
 	}
 
-	err = libAppImage.Register(targetFilePath)
+	err = libAppImage.Register(targetFilePath) // Register The File
 	if err != nil {
 		fmt.Println("Integration failed: " + err.Error())
 	} else {
