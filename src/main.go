cc45e27248e1987072b2f7ff40bcadd6a01ca7f2
ADITYA MISHRA <75035219+DEVLOPRR@users.noreply.github.com>
2022-03-23 04:44:44 +0530

    Update main.go

--- src/main.go
+++ src/main.go
@@ -36,7 +36,7 @@ func main() {
 			Compact: true,
 		}),
 		kong.Vars{
-			"VERSION": "0.4.2",
+			"VERSION": "0.4.3",
 		})
 	// Call the Run() method of the selected parsed command.
 	err := ctx.Run(cli.Debug)
