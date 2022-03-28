2b8b91e056be5fb6c38327ce30fea7c906918fb7
Aditya Mishra <75035219+DEVLOPRR@users.noreply.github.com>
2022-03-28 21:14:34 +0530

    Added `--show-sha1` in List Command For Showing SHA1 Hashes Too

--- src/commands/list.go
+++ src/commands/list.go
@@ -1,16 +1,17 @@
 package commands
 
 import (
-	"bytes"
-	"bread/src/helpers/utils"
 	"os"
-	"path/filepath"
+	"fmt"
 	"sort"
-
+	"bytes"
+	"path/filepath"
+	"bread/src/helpers/utils"
 	"github.com/juju/ansiterm"
 )
 
 type ListCmd struct {
+	ShowSha1 bool `short:"s" name:"show-sha1" help:"Show SHA1 Hashes too." default:"false"`
 }
 
 // Function which will be executed when `list` is called.
@@ -22,19 +23,33 @@ func (r *ListCmd) Run(debug bool) error {
 	defer registry.Close() // Close the registry before function return
 
 	registry.Update() // Update the registry with latest application info
+	if len(registry.Entries) == 0 {
+		fmt.Println("No installed Applications Found!")
+	}
+
 	var buf bytes.Buffer // Buffer which will hold the table
 	tabWriter := ansiterm.NewTabWriter(&buf, 20, 4, 0, ' ', 0)
 	tabWriter.SetColorCapable(true)
 
 	tabWriter.SetForeground(ansiterm.Green)
-	_, _ = tabWriter.Write([]byte("User/Repo\t File Name\t SHA1 HASH\n"))
-	_, _ = tabWriter.Write([]byte("---------\t ---------\t ---------\n"))
+	if r.ShowSha1 {
+		_, _ = tabWriter.Write([]byte("User/Repo\t File Name\t SHA1 HASH\n"))
+		_, _ = tabWriter.Write([]byte("---------\t ---------\t ---------\n"))
+	} else {
+		_, _ = tabWriter.Write([]byte("User/Repo\t File Name\n"))
+		_, _ = tabWriter.Write([]byte("---------\t ---------\n"))
+	}
 
 	tabWriter.SetForeground(ansiterm.DarkGray)
 
 	var lines [][]string
 	for fileName, v := range registry.Entries {
-		line := []string{v.Repo, filepath.Base(fileName), v.FileSha1}
+		var line []string
+		if r.ShowSha1 {
+			line = []string{v.Repo, filepath.Base(fileName), v.FileSha1}
+		} else {
+			line = []string{v.Repo, filepath.Base(fileName)}
+		}
 		lines = append(lines, line)
 	}
 	sort.Slice(lines, func(i int, j int) bool {
@@ -42,7 +57,11 @@ func (r *ListCmd) Run(debug bool) error {
 	})
 
 	for _, line := range lines {
-		_, _ = tabWriter.Write([]byte(line[0] + "\t " + line[1] + "\t " + line[2] + "\n"))
+		if r.ShowSha1 {
+			_, _ = tabWriter.Write([]byte(line[0] + "\t " + line[1] + "\t " + line[2] + "\n"))
+		} else {
+			_, _ = tabWriter.Write([]byte(line[0] + "\t " + line[1] + "\t " + "\n"))
+		}
 	}
 	_ = tabWriter.Flush()
 	_, _ = os.Stdout.Write(buf.Bytes())
